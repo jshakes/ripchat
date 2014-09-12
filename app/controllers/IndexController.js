a0afe8a6dfd7da67ef48eabfb5e536558c07a8c7
James Shakespeare <james@bighuman.com>
2014-09-12 15:02:19 -0400

    demo

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -10,17 +10,8 @@ var io = require('socket.io').listen(1337);
 
 io.sockets.on('connection', function (socket) {
 
-  socket.emit('socketId', socket.id);
-
-  socket.on('joinRoom', function(roomId) {
-    
-    console.log('User joined room');
-    socket.join(roomId);
-  });
   socket.on('newMessage', function(data) {
 
-    var roomId = data.roomId;
-    data.senderId = socket.id;
-    io.sockets.in(roomId).emit('newMessage', data);
+    io.sockets.emit('newMessage', data);
   });
 });
--- assets/js/controller.js
+++ assets/js/controller.js
@@ -3,29 +3,19 @@ Ripchat.Controller = {
 
     var messageCollection = Ripchat.request("messageCollection");
     var socket = Ripchat.request("activeSocket");
-    if(data.senderId === socket.id) {
+    if(data.sender !== socket.id) {
       data.fromSelf = true;
     }
-    // Dispatch the message to the correct room
-    var messageCollection = Ripchat.request("messageCollection", data.roomId);
     messageCollection.add(data);
   },
-  sendNewMessage: function(content, roomId) {
+  sendNewMessage: function(content) {
 
     // Get the currently active socket object
     var socket = Ripchat.request("activeSocket");
     var data = {
       content: content,
-      roomId: roomId,
       sender: $("#username-input").val()
     };
     socket.emit("newMessage", data);
-  },
-  changeRoom: function(roomId) {
-
-    var messagesView = Ripchat.request("messageList", roomId);
-    messagesRegion.show(messagesView);
-    var socket = Ripchat.request("activeSocket");
-    socket.emit("joinRoom", roomId);
   }
 };
